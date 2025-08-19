63acc00c24ead70ae6bfe426efd5cbe304d3c4e7
Joaolucasrebello <joaolucasrebello@gmail.com>
2025-08-19 20:58:01 -0300

    fix(19/08/25)| rename dir

---
+++ POO_LISTAS/Lista1/Questao1/App.java
@@ -0,0 +1,22 @@
+package POO_AULA.Lista1.Questao1;
+
+public class App {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa();
+        p.peso = 50; 
+        p.altura = 1.75; 
+        p.nome = "João";
+
+        System.out.println("O IMC de " + p.nome +  " é: " + p.calcularIMC());
+
+        if (p.calcularIMC() < 18.5) {
+            System.out.println("Abaixo do peso");
+        } else if (p.calcularIMC() >= 18.5 && p.calcularIMC() < 24.9) {
+            System.out.println("Peso normal");
+        } else if (p.calcularIMC() >= 25 && p.calcularIMC() < 29.9) {
+            System.out.println("Sobrepeso");
+        } else {
+            System.out.println("Obesidade");
+        }
+    }
+}
